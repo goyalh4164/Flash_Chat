4bfcc8640ee8a586eb722cd485c396e67b7f0963
harshg77777 <96003201+harshg77777@users.noreply.github.com>
2022-08-21 11:33:45 +0530

    fetching data from firebase and using snapshot streams for quick update

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -33,6 +33,22 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+  //time consuming now using streams
+  // Future<void> getMessages() async {
+  //  final messages= await _firestore.collection('messages').get();
+  //  for (var message in messages.docs){
+  //    print(message.data());
+  //  }
+  // }
+  void messagesStream() async {
+    //it automatically updates the data on the screen as soon as any change occur
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,8 +58,9 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                _auth.signOut();
-                Navigator.pop(context);
+                // _auth.signOut();
+                // Navigator.pop(context);
+                messagesStream();
               }),
         ],
         title: Text('⚡️Chat'),
